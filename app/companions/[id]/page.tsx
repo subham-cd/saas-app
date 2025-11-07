4701a59b3689526080e1bd44853d973486afc17c
Subham Singh <subham0506singh@gmail.com>
2025-11-07 22:36:06 +0530

    Complete this project

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,10 +1,61 @@
-import React from 'react'
+import { getCompanion } from "@/lib/actions/companion.action";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import {getSubjectColor} from "@/lib/utils";
+import Image from "next/image";
+import CompanionComponent from "@/components/CompanionComponent";
 
-const CompanionSession = () => {
+
+
+interface CompanionSessionPageProps{
+  params:Promise<{id:string}>,
+
+}
+
+
+const CompanionSession = async ({params}:CompanionSessionPageProps) => {
+  const {id} = await params;
+  const companion = await getCompanion(id);
+  const user = await currentUser();
+
+  const { name, subject, title, topic, duration } = companion;
+
+  if(!user) redirect('/sign-in');
+  if(!companion) redirect('/companions')
+
+
   return (
-    <div>
-      
-    </div>
+     <main>
+            <article className="flex rounded-border justify-between p-6 max-md:flex-col">
+                <div className="flex items-center gap-2">
+                    <div className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden" style={{ backgroundColor: getSubjectColor(companion.subject)}}>
+                        <Image src={`/icons/${companion.subject}.svg`} alt={companion.subject} width={35} height={35} />
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <div className="flex items-center gap-2">
+                            <p className="font-bold text-2xl">
+                                {companion.name}
+                            </p>
+                            <div className="subject-badge max-sm:hidden">
+                                {companion.subject}
+                            </div>
+                        </div>
+                        <p className="text-lg">{companion.topic}</p>
+                    </div>
+                </div>
+                <div className="items-start text-2xl max-md:hidden">
+                    {companion.duration} minutes
+                </div>
+            </article>
+
+            <CompanionComponent
+                {...companion}
+                companionId={id}
+                userName={user.firstName!}
+                userImage={user.imageUrl!}
+            />
+        </main>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -3,46 +3,37 @@ import CompanionsList from '@/components/CompanionsList'
 import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
 import { recentSessions } from '@/constants'
+import { getAllCompanions, getRecentSessions } from '@/lib/actions/companion.action'
+import { getSubjectColor } from '@/lib/utils'
 
 
-const Page = () => {
+const Page = async () => {
+
+  const companions = await getAllCompanions({limit:3});
+  const recentSessionCompanions = await getRecentSessions(10);
   return (
     <main>
       <h1 >Popular Companions </h1>
        <section className='home-section'>
-        <CompanionCard 
-          id="123"
-          name="Neural the Brainy Explorer"
-          topic="Neural Network of the Brain"
-          subject="science"
-          duration={45}
-          color="#ffda6e"
+
+        {companions.map((companion)=>(
+          <CompanionCard 
+           key={companion.id}
+           {...companion}
+           color={getSubjectColor(companion.subject)}
+         
 
         />
-        <CompanionCard 
-          id="466"
-          name="Countsy the Number wizard"
-          topic="Derivatives & Integrals"
-          subject="Math"
-          duration={30}
-          color="#6eff7f"
 
-        />
-        <CompanionCard 
-          id="564"
-          name="verba the Vocabulary Builder"
-          topic="English Literature"
-          subject="language"
-          duration={30}
-          color="#3cc5ce"
-
-        />
+        ))}
+        
+       
        
        </section>
        <section className='home-section'>
            <CompanionsList
              title="Recently Completed sessions"
-             companions = {recentSessions}
+             companions = {recentSessionCompanions}
              classNames="w-2/3 max-lg:w-full"
            
            
