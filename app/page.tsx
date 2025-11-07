a20c5c13bf7dc9c7dc9a5b39bc2843bade214d04
Subham Singh <subham0506singh@gmail.com>
2025-11-07 23:26:31 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
 import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionsList";
